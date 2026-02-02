5b01340e3b395f00b34dc88ed2545824842e15da
mckinneyjaiden5418 <spiderbeary@gmail.com>
2026-02-02 11:49:04 -0500

    Doubly Linked List implementation.

---
+++ DoublyLinkedList/LinkedList.h
@@ -0,0 +1,25 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+struct Node {
+	int val;
+	Node* next;
+	Node* prev;
+};
+
+
+class LinkedList {
+private:
+	Node* head;
+	Node* tail;
+
+public:
+	LinkedList();
+	void insert_front(int val);
+	void insert_back(int val);
+	void delete_front();
+	void delete_back();
+	void display();
+};
+
+#endif
